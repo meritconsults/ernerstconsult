f4adcecbd75a4de6f00a2bd0793a1f1dd11c1b96
meritconsults <merittravelsconsults@gmail.com>
2025-07-06 16:25:20 +0000

    update the router to use hashrouter

--- merit-consult/src/App.jsx
+++ merit-consult/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import FormApplication from './components/Forms'
-import { BrowserRouter as Router, Routes, Route, Form } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Form } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import FormList from './components/FormList';
